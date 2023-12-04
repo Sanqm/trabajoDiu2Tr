42f3901d157500f0aa0ef71beb17d20f84099d98
Sanqm <sandra.queimadelos@gmail.com>
2023-12-05 00:09:17 +0100

    javaScript

--- js/comprar.js
+++ js/comprar.js
@@ -8,3 +8,38 @@ function showTable(){
     mostrar=true;
   }
 }
+
+
+var mhistoria = true;
+var parrafos =  document.getElementsByClassName('poculto');
+function showH(){
+  if (mhistoria) {
+
+    for (var i = 0; i < parrafos.length; i++) {
+      parrafos[i].style.display = "block";
+    }
+    mhistoria = false;
+  }else{
+    for (var i = 0; i < parrafos.length; i++) {
+      parrafos[i].style.display = "none";
+    }
+    mhistoria = true;
+  }
+}
+
+var mhistoriaB = true;
+var parrafosB =  document.getElementsByClassName('pocultoB');
+function showHB(){
+  if (mhistoriaB) {
+
+    for (var i = 0; i < parrafosB.length; i++) {
+      parrafosB[i].style.display = "block";
+    }
+    mhistoriaB = false;
+  }else{
+    for (var i = 0; i < parrafosB.length; i++) {
+      parrafosB[i].style.display = "none";
+    }
+    mhistoriaB = true;
+  }
+}
